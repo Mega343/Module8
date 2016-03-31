239bf566f8327b8ede2e84fd28308f74cb67f855
артур <mega9594@Rambler.ru>
2016-03-31 14:57:10 +0300

    new realize

---
+++ src/com/goit/gojavaonline/module8/Flower.java
@@ -0,0 +1,34 @@
+package com.goit.gojavaonline.module8;
+
+
+
+public class Flower {
+
+ private    String color;
+ private    int price;
+ private    int lenght;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+}
---
+++ src/com/goit/gojavaonline/module8/RoseShurb.java
@@ -0,0 +1,5 @@
+package com.goit.gojavaonline.module8;
+
+
+public class RoseShurb {
+}
